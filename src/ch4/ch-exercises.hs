fc5859964f5533878645a053777d92bd35708d6a
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2023-03-21 06:54:20 -0500

    feat: ch4 complete

--- src/ch4/ch-exercises.hs
+++ src/ch4/ch-exercises.hs
@@ -1,4 +1,4 @@
-module Exercise8 where
+module ChExercises where
 
   -- Exercise 8
   isPalindrome :: Eq a => [a] -> Bool
@@ -11,3 +11,19 @@ module Exercise8 where
   -- Exercise 10
   f :: (a, b) -> (c, d) -> ((b, d), (a, c))
   f tupleOne tupleTwo = ((snd tupleOne, snd tupleTwo), (fst tupleOne, fst tupleTwo))
+
+
+  -- Correcting Syntax
+
+  -- Exercise 1
+  addOneToString :: String -> Int
+  addOneToString xs = w + 1
+                    where w = length xs
+
+  -- Exercise 2
+  id :: a -> a
+  id x = x
+
+  -- Exercise 3
+  fTwo :: (Integer, Integer) -> Integer
+  fTwo (a, b) = a
--- src/ch4/ch-exercises.md
+++ src/ch4/ch-exercises.md
@@ -18,3 +18,12 @@
    3. Works because `allAwesome` and `awesome` are members of the `Foldable` type class, thus can be called with the `length` function. Reduces to `5`
    4. Works because both expressions, `(8 == 8)` and `('b' < 'a'>)` reduce to expression, `True` and `False` respectively. Reduces to expression `False`
    5. Won't work because `9` is not a valid operand to the boolean conjunction expressions
+
+## Match function names to their types
+
+1. c
+2. b
+3. a
+4. d
+
+

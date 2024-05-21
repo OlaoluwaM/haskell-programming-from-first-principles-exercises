9bc5fc844c1e892f01183d7c2511ddbc2197676c
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-05-21 07:36:00 -0500

    chore: progress with ch 15

---
+++ src/Ch15/Listy.hs
@@ -0,0 +1,9 @@
+module Ch15.Listy where
+
+newtype Listy a = Listy [a] deriving (Eq, Show)
+
+instance Semigroup (Listy a) where
+  (Listy l) <> (Listy l') = Listy $ l <> l'
+
+instance Monoid (Listy a) where
+  mempty = Listy []

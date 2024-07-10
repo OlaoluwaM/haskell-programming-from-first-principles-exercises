b5ce32d9f5661a8b69172ef4d805df8c568644da
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-07-10 08:39:51 -0500

    chore: Progress with Ch16

--- src/Ch16/Playground.hs
+++ src/Ch16/Playground.hs
@@ -1,1 +1,15 @@
 module Ch16.Playground where
+
+data Possibly a = LolNope | Yeppers a deriving (Eq, Show)
+
+instance Functor Possibly where
+    fmap f (Yeppers a) = Yeppers (f a)
+    fmap _ LolNope = LolNope
+
+data Sum a b = First a | Second b deriving (Eq, Show)
+
+instance Functor (Sum a) where
+    fmap f (Second b) = Second (f b)
+    fmap _ (First a) = First a
+
+-- We can apply f to `First` because it's been made a part of the functorial context, that is, the structure we wish to preserve during our transformation
--- test/Ch16/ExFuncInstancesSpec.hs
+++ test/Ch16/ExFuncInstancesSpec.hs
@@ -12,5 +12,17 @@ functorCompose :: (Functor f, Eq (f c)) => (a -> b) -> (b -> c) -> f a -> Bool
 functorCompose f g x = (fmap g . fmap f) x == fmap (g . f) x
 
 spec :: Spec
-spec = describe "Using QuickCheck to validate Functor instances" $ do
+spec = describe "Using QuickCheck to validate Functor instances (check functor laws)" $ do
     prop "Identity a" $ \ida -> functorIdentity ida && functorCompose (+ 1) (* 2) (ida :: Identity Int)
+
+    prop "Pair a" $ \pair -> functorIdentity pair && functorCompose (+ 1) (* 2) (pair :: Pair Int)
+
+    prop "Two a b" $ \twoAB -> functorIdentity twoAB && functorCompose (++ "World") (++ "Hi") (twoAB :: Two Int String)
+
+    prop "Three a b c" $ \threeABC -> functorIdentity threeABC && functorCompose (True ||) not (threeABC :: Three String Int Bool)
+
+    prop "Three' a b" $ \threeAB' -> functorIdentity threeAB' && functorCompose (True ||) not (threeAB' :: Three' String Bool)
+
+    prop "Four a b c d" $ \fourABCD -> functorIdentity fourABCD && functorCompose (subtract 9) (* 12) (fourABCD :: Four String Int Bool Integer)
+
+    prop "Four' a b" $ \fourAB' -> functorIdentity fourAB' && functorCompose (subtract 9) (* 12) (fourAB' :: Four' String Int)
